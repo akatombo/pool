4ffdd7cd0ec0396ea68ed33104e373bf4a3b5d68
Mathieu Gallé-Tessonneau <mathieu.galletessonneau@gmail.com>
2013-09-06 00:39:28 +0200

    remove useless properties component, 0.2.1
    because, after all, it is REALLY useless :l

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 module.exports = ObjectPool;
 
-var properties = require('properties');
 var slice = Array.prototype.slice;
 
 function ObjectPool (ctor, options) {
@@ -40,41 +39,25 @@ ObjectPool.prototype.release = function (instance /*, release args */) {
 	return this;
 };
 
-// ObjectPool.prototype.length = function (newLength) {
-// 	if (!newLength) {
-// 		return this._length;
-// 	} else {
-// 		this._length = newLength;
-
-// 		if (newLength < this.pool.length) {
-// 			this.pool.splice(newLength - 1);
-// 		}
-
-// 		return this;
-// 	}
-// };
-
 ObjectPool.prototype.clear = function () {
 	this.pool = [];
 
 	return this;
 };
 
-properties(ObjectPool.prototype)
-	.default({ configurable: true })
-	.property('acquire').value().define()
-	.property('release').value().define()
-	.property('clear').value().define()
-	.property('length')
-		.getter(function () { return this._length; })
-		.setter(function (newLength) {
-			this._length = newLength;
+Object.defineProperty(ObjectPool.prototype, 'length', {
+	configurable: true,
+	get: function () {
+		return this._length;
+	},
 
-			if (newLength < this.pool.length) {
-				this.pool.splice(newLength - 1);
-			}
+	set: function (newLength) {
+		this._length = newLength;
 
-			return this;
-		})
-		.define()
-;
+		if (newLength < this.pool.length) {
+			this.pool.splice(newLength - 1);
+		}
+
+		return this;
+	}
+});
